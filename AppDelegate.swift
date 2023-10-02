eadfdf9b812ec5dded377d2a9de430c389122f6c
pomeloCheng <pomeloCheng.dev@icloud.com>
2023-10-02 14:52:04 +0800

    修正背景執行不上傳問題
    1.修正背景任務執行條件與邏輯
    在appDelegate中設定
    當執行時間為23:59:00
    若執行當下為今日，則更新今日
    否則更新前一天

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -7,15 +7,27 @@
 
 import UIKit
 import FirebaseCore
+
+import BackgroundTasks
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    let updateDaily = "com.fitbeats.dailytask"
     let healthManager = HealthManager.shared
-
+    var earliestBeginDate: Date?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
-        let _ = EnergyManager.shared //初始化時間生成器
+        let _ = EnergyManager.shared
+        
+        BGTaskScheduler.shared.register(forTaskWithIdentifier: updateDaily, using: nil) { task in
+            guard let updateDatatask = task as? BGAppRefreshTask else { return }
+            // 在這裡執行你的每日背景工作
+            self.performDailyBackgroundTask(updateDatatask)
+        }
+        
+        scheduleDailyBackgroundTask()
         
         ValueTransformer.setValueTransformer(HKWorkoutTransformer(), forName: NSValueTransformerName(rawValue: "HKWorkoutTransformer"))
         healthManager.requestAuthorization { success, error in
@@ -30,21 +42,120 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    
+    func performDailyBackgroundTask(_ task: BGAppRefreshTask) {
+        if isForegroundTaskCompleted == false {
+            let currentDate = Date()
+            let calendar = Calendar.current
+            // 检查当前时间是否在触发时间之后
+            guard let earliestBeginDate = earliestBeginDate else { return }
+            
+            if currentDate > earliestBeginDate {
+                // 如果是的话，将日期回滚到前一天
+                if let previousDay = calendar.date(byAdding: .day, value: -1, to: currentDate) {
+                    // 更新数据时使用前一天的日期
+                    increaseEnergy(date: previousDay)
+                }
+            } else {
+                // 否则，更新数据时使用当前日期
+                increaseEnergy(date: currentDate)
+            }
+            
+            task.setTaskCompleted(success: true)
+        } else {
+            isForegroundTaskCompleted = false
+            task.setTaskCompleted(success: true)
+        }
+    }
+    
+    func scheduleDailyBackgroundTask() {
+        // 創建每日背景任務請求
+        let taskRequest = BGAppRefreshTaskRequest(identifier: updateDaily)
+        
+        let currentDate = Date()
+        let calendar = Calendar.current
+        if let todayDate = calendar.date(bySettingHour: 0, minute: 0, second: 0, of: currentDate) {
+            
+            // 設置每天的特定時間觸發
+            taskRequest.earliestBeginDate = Calendar.current.date(bySettingHour: 23, minute: 59, second: 00, of: todayDate)
+            earliestBeginDate = taskRequest.earliestBeginDate
+            do {
+                // 提交每日背景任務請求
+                try BGTaskScheduler.shared.submit(taskRequest)
+                
+            } catch {
+                print("無法提交每日背景任務請求：\(error.localizedDescription)")
+            }
+        }
+    }
+    
+    private func increaseEnergy(date: Date) {
+        // 在这里将能量值 X 增加 1234
+        
+        
+        HealthManager.shared.readStepCount(for: date)  { step in
+            if step < 1000 {
+                let increaseNumber = 0
+                NotificationCenter.default.post(name: .updateMonster, object: increaseNumber)
+            } else if step < 2000 {
+                let increaseNumber = 3
+                NotificationCenter.default.post(name: .updateMonster, object: increaseNumber)
+            } else if step < 3000 {
+                let increaseNumber = 4
+                NotificationCenter.default.post(name: .updateMonster, object: increaseNumber)
+            } else {
+                let increaseNumber = 5
+                NotificationCenter.default.post(name: .updateMonster, object: increaseNumber)
+            }
+        }
+        
+        
+        HealthManager.shared.readCalories(for: date) { calories, progress, _ in
+            guard let calories = calories , let progress = progress else{
+                NotificationCenter.default.post(name: .failGetData, object: nil)
+                return
+            }
+            if let userCaroPoint = UserDataManager.shared.currentUserData?["CaloriesPoints"] as? Int {
+                let currentEnergy = userCaroPoint
+                let newCaroPoint = currentEnergy + Int(calories)
+                
+                
+                UserDataManager.shared.currentUserData?["CaloriesPoints"] = newCaroPoint
+                UserDataManager.shared.updateUserInfoInFirestore(fieldName: "CaloriesPoints", fieldValue: newCaroPoint)
+                UserDataManager.shared.fetchUserData()
+            }
+            
+            if let userCheckPoint = UserDataManager.shared.currentUserData?["CheckinPoints"] as? Int {
+                
+                if progress >= 1 {
+                    let currentCheckPoint = userCheckPoint
+                    let newCheckPoint = currentCheckPoint + 1
+                    
+                    UserDataManager.shared.currentUserData?["CheckinPoints"] = newCheckPoint
+                    UserDataManager.shared.updateUserInfoInFirestore(fieldName: "CheckinPoints", fieldValue: newCheckPoint)
+                    UserDataManager.shared.fetchUserData()
+                }
+                
+            }
+        }
+    }
+    
 }
+
+
+
 
--- recordVC/EnergyManager.swift
+++ recordVC/EnergyManager.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-
+var isForegroundTaskCompleted = false
 class EnergyManager {
     static let shared = EnergyManager()
     
@@ -28,7 +28,7 @@ class EnergyManager {
         if let todayDate = calendar.date(bySettingHour: 0, minute: 0, second: 0, of: currentDate) {
             
             // 设置触发时间为每天的晚上23:59:59
-            if let endDate = calendar.date(bySettingHour: 23, minute: 59, second: 30, of: todayDate) {
+            if let endDate = calendar.date(bySettingHour: 23, minute: 59, second: 0, of: todayDate) {
                 
                 // 计算时间间隔，这里是计算到指定时间的时间差
                 let timeInterval = endDate.timeIntervalSince(currentDate)
@@ -36,7 +36,7 @@ class EnergyManager {
                 // 如果时间间隔小于等于0，表示今天的触发时间已经过去，将触发时间设置为明天的时间
                 if timeInterval <= 0 {
                     if let tomorrowDate = calendar.date(byAdding: .day, value: 1, to: todayDate),
-                       let nextEndDate = calendar.date(bySettingHour: 23, minute: 59, second: 30, of: tomorrowDate) {
+                       let nextEndDate = calendar.date(bySettingHour: 23, minute: 59, second: 0, of: tomorrowDate) {
                         
                         // 计算到明天触发时间的时间间隔
                         let nextTimeInterval = nextEndDate.timeIntervalSince(currentDate)
@@ -86,7 +86,7 @@ class EnergyManager {
                 
                 UserDataManager.shared.currentUserData?["CaloriesPoints"] = newCaroPoint
                 UserDataManager.shared.updateUserInfoInFirestore(fieldName: "CaloriesPoints", fieldValue: newCaroPoint)
-                
+                UserDataManager.shared.fetchUserData()
             }
             
             if let userCheckPoint = UserDataManager.shared.currentUserData?["CheckinPoints"] as? Int {
@@ -97,13 +97,13 @@ class EnergyManager {
                     
                     UserDataManager.shared.currentUserData?["CheckinPoints"] = newCheckPoint
                     UserDataManager.shared.updateUserInfoInFirestore(fieldName: "CheckinPoints", fieldValue: newCheckPoint)
-                    
+                    UserDataManager.shared.fetchUserData()
                 }
                 
             }
             
         }
-        UserDataManager.shared.fetchUserData()
+        isForegroundTaskCompleted = true
         // 重新启动定时任务
         startTimer()
     }
